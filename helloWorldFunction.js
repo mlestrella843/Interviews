1b9901890c9f750c807c74f8b59cfe3dfc2990e4
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-09-04 21:14:56 -0600

    Creating add two numbers functions

---
+++ helloWorldFunction.js
@@ -0,0 +1,11 @@
+
+const HelloWorld = () => "Hello World MAria Luisa!";
+
+console.log(HelloWorld());
+
+const addTwoNumbers = () => {
+    let number1 = parseInt(document.getElementById("number1").value);
+    let number2 = parseInt(document.getElementById("number2").value);
+    console.log(number1 + number2);
+}
+
