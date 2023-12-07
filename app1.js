b24a06e8630933da9ae461d0b2a0a584c23f966a
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-12-07 09:36:07 -0700

    Exercises

---
+++ app1.js
@@ -0,0 +1,11 @@
+function imprimir(){
+    for( var i = 0; i < 18000 ;  i++){
+        console.log("Imprimio");
+    }
+}
+
+imprimir();
+
+function presionoClick(){
+    console.log("Click en boton");
+}
