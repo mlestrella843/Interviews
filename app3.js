cf8c84b849021d2a5ad5919542da32532d2a8036
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-12-08 15:54:04 -0700

    Objects

--- app3.js
+++ app3.js
@@ -1,4 +1,4 @@
-
+//* PASO POR VALOR = SE ASIGNA LA DIRECCION DE MEMORIA DE ESA VARIABLE.
 //* Paso por valor, los valores se asumen en su asignacion, y son independientes uno del otro.
 var a = 10;
 var b = a;
@@ -11,7 +11,10 @@ a = 20;
 console.log("a:", a);
 console.log("b:", b); // Aqui b se queda con el mismo valor que a tenia al principio, b = 10;
 
-//* Paso por Referencia. Los valores de las variables estan atados, o dependientes una de la otra.
+//* PASO POR REFERENCIA = SE ASIGNA LA DIRECCION DE MEMORIA A OTRA VARIABLE. 
+//* Paso por Referencia. Los valores de las variables estan atados, porque apuntan al mismo lugar de memoria.
+//* En el momento que se hace la asignacion d = c, d esta asumiendo el lugar d memoria que tiene c, por lo tanto
+//* d tendra el valor que tenga c, y viceversa.
 var c = {
     nombre: "Juana"
 }
---
+++ app4.js
@@ -0,0 +1,38 @@
+
+var persona = {
+    nombre: "Juana",
+    apellido: "Estrella",
+    edad: 34,
+    direccion: {
+        calle: "Av. de la Constitución",
+        pais: "republica Dominicana",
+        edificio: {
+            residencial: "Bolivar II",
+            apto: "C-3"
+        }
+    }
+};
+
+console.log(persona.direccion);
+console.log(persona.direccion.pais);
+
+persona.direccion.zipcode = 5001;
+
+console.log(persona.direccion);
+console.log(persona.direccion.edificio.apto);
+
+//* A medida que crecen los objetos internos se puede crear el paso de valor por referencia como
+//* En la clase pasada.
+//* en caso de quisieramos agregar otra propiedad como numero de piso, podemos hacer los siguiente.
+
+//* Para llegar al valor del edificio sin tener que estar escribiendo por puntos hacemos esto
+
+var edificio = persona.direccion.edificio; //* Aqui le pasamos el lugar de memoria donde esta este objeto
+
+console.log(edificio.residencial); //* Aqui vemos que ya con poner la variable edificio ya podemos accesar
+                                    //* directamente las propiedades internas.
+
+//* En caso de que queramos agregar otro atributo hacemos lo siguiente
+edificio.nopiso = "8vo Piso";
+//* Aqui comprobamos que se agrego el atributo de no de piso y que luego de sacarlo por consola aparece todo el objeto edificio.
+console.log(edificio);
